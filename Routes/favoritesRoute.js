8b103fa31f8df3e8f2e1d96e666cbf6dc6e1b2d5
Ali A. Koye <52798572+ARTYOM12321@users.noreply.github.com>
2020-04-14 12:17:41 +0300

    Add files via upload

---
+++ Routes/favoritesRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const Router = express.Router({
+  mergeParams: true
+});
+
+const newController = require('../controllers/favoritesController');
+const authController = require('../controllers/aythController');
+
+/*    authController.protect,
+    authController.restrictTo('admin'),*/
+Router.route('/')
+  .get(
+    //authController.protect
+    authController.isLoggedIn,
+    newController.setUserPostid,
+    newController.getAllfavs
+  )
+  .post(
+    // authController.protect,
+    //   authController.restrictTo('admin'),
+    authController.isLoggedIn,
+    newController.setUserPostid,
+    newController.Createfavs
+  );
+
+Router.route('/:id').delete(
+  authController.isLoggedIn,
+  // authController.protect,
+  // authController.restrictTo('admin'),
+  newController.Deletefavs
+);
+
+module.exports = Router;
